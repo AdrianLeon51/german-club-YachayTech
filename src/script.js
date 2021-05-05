8a92d8856ffe96acd28479546846a30ccbd640a9
AdrianLeon51 <65872008+AdrianLeon51@users.noreply.github.com>
2021-05-04 23:13:23 -0500

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+document.getElementsByTagName("h4")[0].style.fontSize = "2vw";
+document.getElementsByTagName("p")[0].style.fontSize = "2vw";
+document.getElementsByTagName("p")[1].style.fontSize = "2vw";
+document.getElementsByTagName("p")[2].style.fontSize = "2vw";
+document.getElementsByTagName("ul")[0].style.fontSize = "2vw";
